ebfbc743ffd916b3d479b6b38a167c2ea0bd7e09
HellenSabo <hellen.geekmais@gmail.com>
2024-10-11 17:01:59 -0300

    Commit inicial da API  Tapidindo Tecnologia

---
+++ src/main/java/com/tapidindo/TapidindoApiApplication.java
@@ -0,0 +1,11 @@
+package com.tapidindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapidindoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TapidindoApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tapidindo/TapidindoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapidindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapidindoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
